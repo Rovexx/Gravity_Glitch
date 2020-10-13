5588a8c0ea7090ec821f9b189f3f6c9f73ae6996
Dani <dani.smits@outlook.com>
2020-10-13 16:58:16 +0200

    charactermovement

---
+++ Gravity Glitch/Assets/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private PlayerInputActions inputActions;
+    private Vector2 lookPosition;
+    public float xRotation = 0f;
+    public float rotateSensitivity = 100f;
+    public Transform player;
+    void Awake()
+    {
+        inputActions = new PlayerInputActions();
+        inputActions.Player.Look.performed += value => lookPosition = value.ReadValue<Vector2>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+    }
+    void OnEnable()
+    {
+        inputActions.Enable();
+    }
+    void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    private void Rotate()
+    {
+        float mouseX = lookPosition.x * rotateSensitivity * Time.deltaTime;
+        float mouseY = lookPosition.y * rotateSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0);
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Gravity Glitch/Assets/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    public PlayerInputActions inputActions;
+    public Vector2 movementVector;
+    public float movementSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Awake()
+    {
+        inputActions = new PlayerInputActions();
+        inputActions.Player.Move.performed += value => movementVector = value.ReadValue<Vector2>();
+        inputActions.Player.RotateE.performed += RotateE_performed;
+        inputActions.Player.RotateQ.performed += RotateQ_performed;
+    }
+
+    private void RotateQ_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        transform.Rotate(-90f,0f,0f);
+    }
+
+    private void RotateE_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        transform.Rotate(90f,0f,0f);
+    }
+
+    void OnEnable()
+    {
+        inputActions.Enable();
+    }
+    void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = movementVector.x;
+        float vertical = movementVector.y;
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        Move(direction);
+    }
+
+    private void Move(Vector3 moveDirection)
+    {
+        if (moveDirection.magnitude >= 0.1f)
+        {
+            Vector3 relativeDirection = transform.right * moveDirection.x + transform.forward * moveDirection.z;
+            Vector3 movement = relativeDirection * movementSpeed * Time.fixedDeltaTime;
+
+            _rigidbody.MovePosition(transform.position + movement);
+        }
+    }
+}
